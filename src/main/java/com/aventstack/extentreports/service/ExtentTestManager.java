451ac05a8aa9b6a32f25b42b0b85d94f36dcab31
anshooarora <anshooarora@gmail.com>
2018-11-17 23:32:00 -0500

    externalize test creation, allows exposing the current test globally

---
+++ src/main/java/com/aventstack/extentreports/service/ExtentTestManager.java
@@ -0,0 +1,93 @@
+package com.aventstack.extentreports.service;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.model.Test;
+import com.aventstack.extentreports.testng.listener.commons.ExtentTestCommons;
+
+public class ExtentTestManager {
+
+	private static Map<String, ExtentTest> classTestMap = new HashMap<>();
+	private static ThreadLocal<ExtentTest> methodTest = new ThreadLocal<>();
+	private static ThreadLocal<ExtentTest> dataProviderTest = new ThreadLocal<>();
+
+	public static synchronized ExtentTest getTest() {
+		return methodTest.get();
+	}
+
+	public static synchronized ExtentTest getTest(ITestResult result) {
+		ExtentTest t = result.getParameters() != null && result.getParameters().length > 0 ? dataProviderTest.get()
+				: methodTest.get();
+		return t;
+	}
+
+	public static synchronized ExtentTest createMethod(ITestResult result, Boolean createAsChild) {
+		if (!createAsChild)
+			return createMethod(result);
+
+		String className = result.getInstance().getClass().getSimpleName();
+		String methodName = result.getMethod().getMethodName();
+		ExtentTest classTest;
+
+		if (classTestMap.containsKey(className)) {
+			classTest = classTestMap.get(className);
+		} else {
+			classTest = ExtentService.getInstance().createTest(className);
+			classTestMap.put(className, classTest);
+		}
+
+		Optional<Test> test = classTest.getModel().getNodeContext().getAll().stream()
+				.filter(x -> x.getName().equals(methodName)).findFirst();
+
+		if (result.getParameters().length > 0) {
+			if (!test.isPresent()) {
+				createTest(result, classTest);
+			}
+			String paramName = Arrays.asList(result.getParameters()).toString();
+			ExtentTest paramTest = methodTest.get().createNode(paramName);
+			dataProviderTest.set(paramTest);
+		} else {
+			dataProviderTest.set(null);
+			createTest(result, classTest);
+		}
+
+		return methodTest.get();
+	}
+
+	public static synchronized ExtentTest createMethod(ITestResult result) {
+		String methodName = result.getMethod().getMethodName();
+		if (result.getParameters().length > 0) {
+			if (methodTest.get() != null && methodTest.get().getModel().getName().equals(methodName)) {
+			} else {
+				createTest(result, null);
+			}
+			String paramName = Arrays.asList(result.getParameters()).toString();
+			ExtentTest paramTest = methodTest.get().createNode(paramName);
+			dataProviderTest.set(paramTest);
+		} else {
+			createTest(result, null);
+		}
+		return methodTest.get();
+	}
+
+	private static synchronized ExtentTest createTest(ITestResult result, ExtentTest classTest) {
+		String methodName = result.getMethod().getMethodName();
+		ExtentTest test;
+		if (classTest != null) {
+			test = classTest.createNode(methodName, result.getMethod().getDescription());
+		} else {
+			test = ExtentService.getInstance().createTest(methodName, result.getMethod().getDescription());
+		}
+		methodTest.set(test);
+		String[] groups = result.getMethod().getGroups();
+		ExtentTestCommons.assignGroups(test, groups);
+		return test;
+	}
+
+}
--- src/main/java/com/aventstack/extentreports/testng/listener/ExtentITestListenerClassAdapter.java
+++ src/main/java/com/aventstack/extentreports/testng/listener/ExtentITestListenerClassAdapter.java
@@ -1,101 +1,47 @@
 package com.aventstack.extentreports.testng.listener;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import com.aventstack.extentreports.AnalysisStrategy;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.model.Test;
 import com.aventstack.extentreports.service.ExtentService;
-import com.aventstack.extentreports.testng.listener.commons.ExtentTestCommons;
+import com.aventstack.extentreports.service.ExtentTestManager;
 
-public class ExtentITestListenerClassAdapter 
-    implements ITestListener {
+public class ExtentITestListenerClassAdapter implements ITestListener {
 
-    private static Map<String, ExtentTest> classTestMap = new HashMap<>();
-    private static ThreadLocal<ExtentTest> methodTest = new ThreadLocal<>();
-    private static ThreadLocal<ExtentTest> dataProviderTest = new ThreadLocal<>();
-    
-    @Override
-    public synchronized void onStart(ITestContext context) { 
-        ExtentService.getInstance().setAnalysisStrategy(AnalysisStrategy.CLASS);
-    }
+	@Override
+	public synchronized void onStart(ITestContext context) {
+		ExtentService.getInstance().setAnalysisStrategy(AnalysisStrategy.CLASS);
+	}
 
-    @Override
-    public synchronized void onFinish(ITestContext context) {
-        ExtentService.getInstance().flush();
-    }
-    
-    @Override
-    public synchronized void onTestStart(ITestResult result) {
-        String className = result.getInstance().getClass().getSimpleName();
-        String methodName = result.getMethod().getMethodName();
-        ExtentTest classTest;
-        
-        if (classTestMap.containsKey(className)) {
-            classTest = classTestMap.get(className);
-        } else {
-            classTest = ExtentService.getInstance().createTest(className);
-            classTestMap.put(className, classTest);
-        }
+	@Override
+	public synchronized void onFinish(ITestContext context) {
+		ExtentService.getInstance().flush();
+	}
 
-        Optional<Test> test = classTest.getModel()
-            .getNodeContext().getAll()
-            .stream()
-            .filter(x -> x.getName().equals(methodName))
-            .findFirst();
-        
-        if (result.getParameters().length>0) {
-            if (!test.isPresent()) {
-                createNode(classTest, result);
-            }
-            String paramName = Arrays.asList(result.getParameters()).toString();
-            ExtentTest paramTest = methodTest.get().createNode(paramName);
-            dataProviderTest.set(paramTest);
-        } else {
-            dataProviderTest.set(null);
-            createNode(classTest, result);
-        }
-    }
+	@Override
+	public synchronized void onTestStart(ITestResult result) {
+		ExtentTestManager.createMethod(result, true);
+	}
 
-    private void createNode(ExtentTest classTest, ITestResult result) {
-        String methodName = result.getMethod().getMethodName();
-        ExtentTest child = classTest.createNode(methodName, result.getMethod().getDescription());
-        methodTest.set(child);
-        
-        String[] groups = result.getMethod().getGroups();
-        ExtentTestCommons.assignGroups(child, groups);
-    }
-    
-    @Override
-    public synchronized void onTestSuccess(ITestResult result) {
-        getTest(result).pass("Test passed");
-    }
-    
-    private ExtentTest getTest(ITestResult result) {
-        ExtentTest t = result.getParameters() != null && result.getParameters().length>0
-                ? dataProviderTest.get()
-                : methodTest.get();
-        return t;
-    }
+	@Override
+	public synchronized void onTestSuccess(ITestResult result) {
+		ExtentTestManager.getTest(result).pass("Test passed");
+	}
 
-    @Override
-    public synchronized void onTestFailure(ITestResult result) {
-        getTest(result).fail(result.getThrowable());
-    }
+	@Override
+	public synchronized void onTestFailure(ITestResult result) {
+		ExtentTestManager.getTest(result).fail(result.getThrowable());
+	}
 
-    @Override
-    public synchronized void onTestSkipped(ITestResult result) {
-        getTest(result).skip(result.getThrowable());
-    }
+	@Override
+	public synchronized void onTestSkipped(ITestResult result) {
+		ExtentTestManager.getTest(result).skip(result.getThrowable());
+	}
 
-    @Override
-    public synchronized void onTestFailedButWithinSuccessPercentage(ITestResult result) { }
+	@Override
+	public synchronized void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+	}
 
 }
